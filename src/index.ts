660c1e423f6a6f404ff5687620e2256a077e062d
Vitaliy19891712 <stopkaiko@mail.ru>
2023-09-27 21:52:15 +0300

    EWQ

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ app.get("/", (req: Request, res: Response) => {
   const text = "Hello 1!";
   res.send(text);
 });
-app.get("/qwerty", (req, res) => {
-  res.send("qwerty");
+app.post("/QWEQWE", async (req, res) => {
+  res.sendStatus(200);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
