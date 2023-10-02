c97bbff9997dc195b26814e53010f05c6443e4a3
Vitaliy19891712 <stopkaiko@mail.ru>
2023-10-02 19:54:28 +0300

    add get query

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+const products = [{ title: "tomato" }, { title: "orange" }];
+const adresses = [{ value: "Moscow" }, { value: "USA" }];
+
 app.get("/", (req: Request, res: Response) => {
-  const text = "Hello 123!";
+  const text = "Hello 1!";
   res.send(text);
+});
+
+app.get("/products", (req: Request, res: Response) => {
+  res.send(products);
+});
+
+app.get("/adresses", (req: Request, res: Response) => {
+  res.send(adresses);
 });
 
 app.listen(port, () => {
