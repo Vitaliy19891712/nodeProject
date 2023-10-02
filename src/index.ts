cedc9f68b5e7e6ea67dae69dfd1742696b1a2776
Vitaliy19891712 <stopkaiko@mail.ru>
2023-10-02 20:57:04 +0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const products = [{ title: "tomato" }, { title: "orange" }];
-const adresses = [{ value: "Moscow" }, { value: "USA" }];
+const products = [
+  { id: 1, title: "tomato" },
+  { id: 2, title: "orange" },
+];
+const adresses = [
+  { id: 1, value: "Moscow" },
+  { id: 2, value: "USA" },
+];
 
 app.get("/", (req: Request, res: Response) => {
   const text = "Hello 1!";
@@ -12,18 +18,51 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/products", (req: Request, res: Response) => {
-  res.send(products);
+  if (req.query.title) {
+    const searchString = req.query.title?.toString();
+    res.send(products.filter((p) => p.title.indexOf(searchString)));
+  } else {
+    res.send(products);
+  }
 });
 
-app.get("/products/:productTitle", (req: Request, res: Response) => {
-  let product = products.find((prod) => prod.title === req.params.productTitle);
-  res.send(product);
+app.get("/products/:id", (req: Request, res: Response) => {
+  let product = products.find((prod) => prod.id === +req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.send(404);
+  }
 });
 
 app.get("/adresses", (req: Request, res: Response) => {
   res.send(adresses);
 });
 
+app.get("/adresses/:id", (req: Request, res: Response) => {
+  let adress = adresses.find((adr) => adr.id === +req.params.id);
+  if (adress) {
+    res.send(adress);
+  } else {
+    res.send(404);
+  }
+});
+
+app.delete("/products/:id", (req: Request, res: Response) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === +req.params.id) {
+      products.splice(i, 1);
+      res.send(204);
+      return;
+    }
+  }
+  res.send(404);
+});
+app.post("/products", (req: Request, res: Response) => {
+  const newProduct = { id: +new Date(), title: req.body.title };
+  products.push(newProduct);
+  res.status(201).send(newProduct);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
